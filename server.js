61669a58fd18c420555b32013005eac595d4ed46
Ryan Petschek <petschekr@gmail.com>
2014-09-19 19:43:57 -0400

    Initial website commit

---
+++ server.js
@@ -0,0 +1,47 @@
+/// <reference path="typescript_defs/node.d.ts" />
+var crypto = require("crypto");
+
+var express = require("express");
+var app = express();
+
+// Express middleware
+var serveStatic = require("serve-static");
+var responseTime = require("response-time");
+var bodyParser = require("body-parser");
+var compress = require("compression");
+
+app.use(compress());
+app.use(bodyParser());
+app.use(responseTime());
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+app.locals.pretty = true;
+
+app.use("/css", serveStatic("css"));
+app.use("/js", serveStatic("js"));
+
+function createNonce(bytes) {
+    if (typeof bytes === "undefined") { bytes = 32; }
+    return crypto.randomBytes(bytes).toString("hex");
+}
+
+app.route("/").get(function (request, response) {
+    response.render("index", function (err, html) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        response.send(html);
+    });
+});
+
+// 404 page
+app.use(function (request, response, next) {
+    response.send(404, "404 Not found!");
+});
+
+var PORT = 8080;
+app.listen(PORT, "0.0.0.0", function () {
+    console.log("Server listening on port " + PORT);
+});
